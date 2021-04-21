8e2c56cba3cc80381ed1fcf79011ea92b0884321
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-04-20 18:26:56 -0700

    Created contact page. It's basically done

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -1,51 +1,65 @@
-/* Modal View */
-const modal = document.querySelector('.modalView');
-const modalBtn = document.querySelector('.modalBtn');
-const modalFocus = document.querySelector('.modalFocus');
-// nodelist of focusable elems to obscure while in modal view
-const trapList = document.querySelectorAll('.trapFocus');
-const trapFocus = Array.from(trapList);
-// hide/reveal modal, make other elems reachable/unreachable
-const modalToggle = () => {
-  modal.classList.toggle('hide');
-  !trapFocus[0].hasAttribute('tabindex')
-    ? trapFocus.forEach((e) => e.setAttribute('tabindex', '-1'))
-    : trapFocus.forEach((e) => e.removeAttribute('tabindex'));
-  modalFocus.focus();
-};
 /* email validator */
 const validator = (email) => {
   const valid = /\S+@\S+\.\S+/;
   return valid.test(email);
 };
+
+/* show confirmation */
+const confirmCard = document.getElementById('confirm');
+const confirmFocus = document.querySelector('#confirm p');
+
+const confirmSubmission = () => {
+  confirmCard.setAttribute('aria-hidden', 'false');
+  confirmCard.style.display = 'block';
+  confirmFocus.tabIndex = '0';
+  confirmFocus.focus();
+};
+
+/* hide confirmation */
+const contactName = document.getElementById('contact-name');
+const contactEmail = document.getElementById('contact-email');
+const contactMsg = document.getElementById('contact-message');
+const lastName = document.getElementById('last-name');
+
+const hideSubmission = () => {
+  confirmCard.setAttribute('aria-hidden', 'true');
+  confirmCard.style.display = 'none';
+  confirmFocus.tabIndex = '-1';
+};
+
+const formFields = [];
+formFields.push(contactName, contactEmail, contactMsg, lastName);
+
+formFields.forEach((field) => {
+  field.addEventListener('focus', hideSubmission);
+});
+
 /* Google Sheets API */
 const scriptURL =
   'https://script.google.com/macros/s/AKfycbzTAGlHnrDCOfTo_uVvXPDldwTjIWI2PDJ0Bu6bw8f5-4lCrb6u42fP/exec';
-const form = document.forms['submitToSheet'];
-/* send msg to Google Sheets & triggers modal */
+const form = document.forms['submit-to-sheet'];
+
 const storeInfo = (e) => {
-  const contactName = document.getElementById('contactName').value;
-  const lastName = document.getElementById('lastName').value;
-  const contactEmail = document.getElementById('contactEmail').value;
-  const contactMsg = document.getElementById('contactMessage').value;
-  // validate, apply/remove helpers, check for lastName hit API
-  if (contactName === '' || contactEmail === '' || contactMsg === '') {
+  if (
+    contactName.value === '' ||
+    contactEmail.value === '' ||
+    contactMsg.value === ''
+  ) {
     return;
-  } else if (!validator(contactEmail)) {
+  } else if (!validator(contactEmail.value)) {
     return;
-  } else if (lastName) {
-    modalToggle();
+  } else if (lastName.value) {
+    confirmSubmission();
     form.reset();
   } else {
     fetch(scriptURL, { method: 'POST', mode: 'cors', body: new FormData(form) })
       .then((response) => console.log('Success!', response))
       .catch((error) => console.error('Error!', error.message));
-    modalToggle();
+    confirmSubmission();
     e.preventDefault(); // prevents additional html validation after submit
     form.reset();
   }
 };
-/* listeners */
+
 const submit = document.getElementById('submit');
 submit.addEventListener('click', storeInfo);
-modalBtn.addEventListener('click', modalToggle);
