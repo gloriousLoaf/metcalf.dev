0241a5ae69f97b4e5836baa4176347a2ba352dfa
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-05-03 18:09:57 -0700

    Upgraded theme switching: added media query to check for dark theme & serve that instead, with 'Theme' button toggling to light mode for those users. Switching system preference back to light works too. Swapping out octocat logo based on theme is now handled by css instead of js. See new scheme.css file.

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,17 +1,14 @@
 /* Theme Switcher */
 const box = document.querySelector('.box');
 const themeBtn = document.getElementById('theme-btn');
-const ghLogo = document.getElementById('gh-logo');
 
 const lightMode = () => {
   box.classList.remove('invert');
-  ghLogo.src = './assets/images/gh-dark.png';
   localStorage.setItem('dark theme', 'off');
 };
 
 const darkMode = () => {
   box.classList.add('invert');
-  ghLogo.src = './assets/images/gh-light.png';
   localStorage.setItem('dark theme', 'on');
 };
 
